e9558c1c64b1c9601b939e95b0ee1f1bb9d03d9a
jerryaoverton <jerryaoverton@yahoo.com>
2017-01-24 00:10:17 -0600

    Add files via upload

---
+++ financial-institution/sim Financial Institute strategy.R
@@ -0,0 +1,85 @@
+#What if a financial institution were willing to make comprehensive changes to 
+#the services they provide to customers?
+
+#read the strategy file for analysis
+strategy <- read.csv("financial.institute.businessmodel.csv")
+
+#calculate the differences that represent competition in different areas
+#of the business model
+strategy$d.cns <- strategy$cns1 - strategy$cns2
+strategy$d.pcn <- strategy$pcn1 - strategy$pcn2
+strategy$d.inv <- strategy$inv1 - strategy$inv2
+strategy$d.imt <- strategy$imt1 - strategy$imt2
+strategy$d.ped <- strategy$ped1 - strategy$ped2
+strategy$d.oe <- strategy$oe1 - strategy$oe2
+strategy$d.cpex <- strategy$cpex1 - strategy$cpex2
+strategy$d.pro <- strategy$pro1 - strategy$pro2
+strategy$d.ikp <- strategy$ikp1 - strategy$ikp2
+
+#rename the variables to correspond to the new offering strategy
+cols <- c("competitor loan customers", "loan customers",
+          "competitor potential loan customers", "potential loan customers",
+          "competitor repayment options", "repayment options",
+          "competitor customer advocacy", "customer advocacy",
+          "competitor necessity to customers", "necessity to customers",
+          "competitor returns to scale", "returns to scale",
+          "competitor capital investment", "capital investment",
+          "competitor time to new services", "time to new services",
+          "competitor partner network efficiency", "employer network efficiency",
+          "fitness",
+          "competitor advantage in loan customers",
+          "competitor advantage in potential loan customers",
+          "competitor advantage in repayment options", 
+          "competitor advantage in customer advocacy",
+          "competitor advantage in necessity to customers", 
+          "competitor advantage in returns to scale",
+          "competitor advantage in capital investment", 
+          "competitor advantage in time to new services",
+          "competitor advantage in partner network efficiency")
+colnames(strategy) <- cols
+
+#discretize all values so that we can create association rules
+ranges <- c("1-Very Low", "2-Low", "3-Medium", "4-High", "5-Very High")
+for(i in 1:ncol(strategy)){
+  strategy[,i] <- cut(strategy[,i], breaks=5, labels = ranges)
+}
+
+#write out the hospital strategy for additional analysis
+write.csv(strategy, file="financial.institute.strategy.csv", row.names=FALSE)
+
+#overall is this a winning strategy?
+barplot(table(strategy$fitness))
+
+#find circumstances that lead to a losing strategy
+library(arules)
+strategy.lose.rules <- apriori(strategy, control = list(verbose=F),
+                               parameter = list(minlen=2, supp=0.035, conf=0.45),
+                               appearance = list(rhs=c("fitness=1-Very Low"),
+                                                 default="lhs"))
+inspect(strategy.lose.rules)
+
+#find redundant strategy rules
+subset.matrix <- is.subset(strategy.lose.rules, strategy.lose.rules)
+subset.matrix[lower.tri(subset.matrix, diag=T)] <- NA
+redundant <- colSums(subset.matrix, na.rm=T) >= 1
+which(redundant)
+
+#remove redundant strategy rules
+strategy.lose.rules.pruned <- strategy.lose.rules[!redundant]
+inspect(strategy.lose.rules.pruned)
+
+#visualize observation rules
+#install.packages("arulesViz")
+library(arulesViz)
+plot(strategy.lose.rules.pruned, method="paracoord")
+
+#find winning strategies when the competitor potential consumers and advantage in
+#potential consumers is high
+#strategy.scenario1 <- strategy[strategy$`competitor potential loan customers` == "4-High",]
+strategy.scenario1 <- strategy[strategy$`competitor advantage in potential loan customers` == "4-High",]
+
+#given the scenario, find the strategies that lead to a win
+library(party)
+formula <- fitness ~ .
+dtree <- ctree(formula, data=strategy.scenario1, controls = ctree_control(maxdepth = 2))
+plot(dtree)
---
+++ financial-institution/sim Financial Institute.R
@@ -0,0 +1,168 @@
+#simulate new possibilities for a better financial institute
+
+#read observed customer experiences
+#experiences <- read.csv("consumer_complaints.csv")
+
+#filter the observations down to only the necessary columns
+#cols <- c("Product", "Sub.issue", "Company")
+#experiences.sub <- experiences[,cols]
+
+#filter the observations to include only student loans
+#studentloans <- experiences.sub[experiences.sub$Product=="Student loan",]
+#write.csv(file = "student_loan_complaints.csv",studentloans)
+
+#read student loan complaints
+studentloans <- read.csv("student_loan_complaints.csv")
+
+#pivot the data to show complaints by company
+#install.packages("reshape")
+library(reshape)
+complaints <- cast(studentloans, Company~Sub.issue, length, value = "X")
+
+#now that the data is pivoted by company, the company name is no longer needed
+#and interferes with subsequent analysis
+complaints$Company <- NULL
+
+#use the real complaints to create a set of simulated complaints
+#start by creating a correlation matix of the observed complaints
+complaints.cor <- cor(complaints)
+
+#start the simulation as a set of random, guaussian variable 
+#correlated according to the matrix
+require(mvtnorm)
+simulation_runs = 3000
+number_of_features = 14
+feature_means <- rep(0,number_of_features)
+complaints.sim <- rmvnorm(mean=feature_means,sig=complaints.cor,n=simulation_runs)
+
+#rename the simulation variables to match the observations
+colnames(complaints.sim) <- colnames(complaints)
+
+#convert the simulation variables to a uniform distribution
+complaints.sim <- pnorm(complaints.sim)
+complaints.sim <- as.data.frame(complaints.sim)
+
+#determine the distribution of each observed variable
+#install.packages("fitdistrplus")
+library(fitdistrplus)
+descdist(complaints[,1], discrete = FALSE) #beta
+descdist(complaints[,2], discrete = FALSE) #beta
+descdist(complaints[,3], discrete = FALSE) #beta
+descdist(complaints[,4], discrete = FALSE) #beta
+descdist(complaints[,5], discrete = FALSE) #beta
+descdist(complaints[,6], discrete = FALSE) #beta
+descdist(complaints[,7], discrete = FALSE) #beta
+descdist(complaints[,8], discrete = FALSE) #beta
+descdist(complaints[,9], discrete = FALSE) #beta
+descdist(complaints[,10], discrete = FALSE) #beta
+descdist(complaints[,11], discrete = FALSE) #beta
+descdist(complaints[,12], discrete = FALSE) #beta
+descdist(complaints[,13], discrete = FALSE) #beta
+descdist(complaints[,14], discrete = FALSE) #beta
+
+#fit all simulated activities to a beta distribution
+library(scales)
+for (i in 1: ncol(complaints.sim)){
+  parm <- fitdist(rescale(complaints[,i],c(0.01,.99)),"beta")
+  complaints.sim[,i] <- qbeta(complaints.sim[,i],
+                                  shape1 = parm$estimate[1],
+                                  shape2 = parm$estimate[2])
+  #rescale the simulation data to match the scale of the observations
+  complaints.sim[,i] <- rescale(complaints.sim[,i],
+                                    c(min(complaints[,i]),
+                                      max(complaints[,i]))
+  )
+}
+
+#the minimum cluster performance threshold
+threshold = .98
+
+#by trial and error, determine the minimum number of clusters needed to reach a
+#given performance threshold. then use that number to cluster the observed complaints
+
+score = 0
+num_clusters = 0
+cluster_performance_below_threshold <- (score < threshold)
+
+while(cluster_performance_below_threshold){
+  num_clusters = num_clusters + 1
+  clusters <- kmeans(complaints, num_clusters)
+  
+  score <- clusters$betweenss/clusters$totss
+  cluster_performance_below_threshold <- (score < threshold)
+}
+complaints$cluster <- clusters$cluster
+
+#by trial and error, determine the minimum number of clusters needed to reach a
+#given performance threshold. then use that number to cluster the simulated complaints
+score = 0
+num_clusters = 0
+cluster_performance_below_threshold <- (score < threshold)
+
+while(cluster_performance_below_threshold){
+  num_clusters = num_clusters + 1
+  clusters <- kmeans(complaints.sim, num_clusters,iter.max=30)
+  
+  score <- clusters$betweenss/clusters$totss
+  cluster_performance_below_threshold <- (score < threshold)
+}
+complaints.sim$cluster <- clusters$cluster
+
+#visualize the distribution of complaint clusters
+hist(complaints$cluster)
+hist(complaints.sim$cluster)
+
+#aggregate the observed and simulated complaints by cluster
+complaints.agg <- aggregate(. ~ cluster, data=complaints, mean)
+complaints.agg$Sum <- rowSums(complaints.agg)
+
+complaints.sim.agg <- aggregate(. ~ cluster, data=complaints.sim, mean)
+complaints.sim.agg$Sum <- rowSums(complaints.sim.agg)
+
+#define the minum and maximum total number of complaints
+complaints.min = 650
+complaints.max = 750
+complaints.sim.min = 450
+complaints.sim.max = 550
+
+#filter the observed and simulated clustered complaints to only those 
+#below a particular threshold of total complaints
+complaints.agg <- complaints.agg[complaints.agg$Sum > complaints.min,]
+complaints.agg <- complaints.agg[complaints.agg$Sum < complaints.max,]
+
+complaints.sim.agg <- complaints.sim.agg[complaints.sim.agg$Sum > complaints.sim.min,]
+complaints.sim.agg <- complaints.sim.agg[complaints.sim.agg$Sum < complaints.sim.max,]
+
+#format the aggregated observed and simulated complaints so that they can 
+#be displayed using to radar chart
+complaints.agg$Company <- NULL
+complaints.agg$cluster <- NULL
+complaints.agg$Sum <- NULL
+labels <- colnames(complaints.agg)
+complaints.agg <- as.data.frame(t(complaints.agg))
+
+complaints.sim.agg$Company <- NULL
+complaints.sim.agg$cluster <- NULL
+complaints.sim.agg$Sum <- NULL
+labels <- colnames(complaints.sim.agg)
+complaints.sim.agg <- as.data.frame(t(complaints.sim.agg))
+
+#display observations and simulations on a single radar chart
+
+#name the column headings for the observed complaints
+name <- rep("obs", ncol(complaints.agg))
+number <- c(1:ncol(complaints.agg))
+col_headings <- paste(name,number, sep = "-")
+names(complaints.agg) <- col_headings
+
+#name the column headings for the simulated complaints
+name <- rep("sim", ncol(complaints.sim.agg))
+number <- c(1:ncol(complaints.sim.agg))
+col_headings <- paste(name,number, sep = "-")
+names(complaints.sim.agg) <- col_headings
+
+#combine and display both observed and simulated complaints
+install.packages("radarchart")
+library(radarchart)
+complaints.tot <- cbind(complaints.agg, complaints.sim.agg)
+chartJSRadar(complaints.tot, maxScale = 170, labs = labels, showToolTipLabel=TRUE)
